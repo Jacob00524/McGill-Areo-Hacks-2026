07d48cd02b39fa79da1ec5ec91f42886e22dd67e
jacob <jakhalifa@outlook.com>
2026-03-14 16:38:42 -0400

    world's second smallest commit

--- test/0_cam/main.py
+++ test/0_cam/main.py
@@ -1,3 +1,18 @@
 import drone 
+import time
 
 print(drone.get_firmware_version())
+
+drone.green_LED(0)
+
+time.sleep(1)
+
+drone.green_LED(1)
+
+time.sleep(2)
+
+drone.green_LED(0)
+
+time.sleep(1)
+
+drone.green_LED(1)
